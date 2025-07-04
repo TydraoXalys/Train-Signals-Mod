4eabf652e2db5d77ce0428d7dd980584cfc577ec
TydraoXalys <quentin.lenfant63@gmail.com>
2025-07-04 09:45:53 +0200

    [0.1.0] Initial commit

---
+++ src/client/java/fr/tydraoxalys/trainsignals/TrainSignalsClient.java
@@ -0,0 +1,10 @@
+package fr.tydraoxalys.trainsignals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrainSignalsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
